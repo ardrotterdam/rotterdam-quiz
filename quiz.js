acd5b98f5c79701937c6a428977a055bda68c652
ardrotterdam <ardrotterdam@gmail.com>
2025-05-29 20:18:02 +0200

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -237,4 +237,13 @@
             }, 300);
         }
 
-       
+        // Prevent cheating
+        document.addEventListener('contextmenu', function(e) {
+            e.preventDefault();
+        });
+
+        document.addEventListener('keydown', function(e) {
+            if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && e.key === 'I')) {
+                e.preventDefault();
+            }
+        });
